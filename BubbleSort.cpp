db8149e1e613c3809ae37e8ac61ccd615f963a38
vuquangtuan123 <vuquangtuan12345@gmail.com>
2020-04-20 15:31:31 +0700

    fix errors + add notes

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 using namespace std;
-void InputArray(float [],int &);
-void OutputArray(float [],int);
-void swap(int,int);
-void BubbleSort(float [],int );
+void InputArray(int [],int &); //tham chiếu
+void OutputArray(int [],int);
+void Swap(int &,int &); //tham chiếu
+void BubbleSort(int [],int );
 
 int main()
 {
-    float a[100];
+    int a[100];
     int n;
     InputArray(a,n);
+    cout<<"Array: ";
     OutputArray(a,n);
     BubbleSort(a,n);
-    cout<<"duoc sap xep thanh ";
+    cout<<"Sorted Array ";
     OutputArray(a,n);
     return 0;
 }
 
-void InputArray(float a[],int &n)
+void InputArray(int a[],int &n) //n là tham chiếu
 {
-    cout<<"Input n ";
-    cin>>n;
+    do{                   //Thực hiện câu lệnh trước 1 lần xong mới xét ĐK
+        cout<<"Input n ";
+        cin>>n;
+    }while(n<=0);     
     for(int i=0;i<n;i++)
     {
-        cout<<"Nhap a["<<i<<"]: ";
+        cout<<"Input a["<<i<<"]: ";
         cin>>a[i];
     }
 }
 
-void OutputArray(float a[],int n)
+void OutputArray(int a[],int n)
 {
     for(int i=0;i<n;i++)
     {
@@ -36,19 +39,19 @@ void OutputArray(float a[],int n)
     }
 }
 
-void swap(int a ,int b)
+void Swap(int &a ,int &b) // Truyền đối số cho hàm là tham chiếu
 {
         int temp=a;
         a=b;
         b=temp;
 }
 
-void BubbleSort(float a[],int n)
+void BubbleSort(int a[],int n) //Sort từ LỚN đến NHỎ
 {
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++)  // Vòng lặp ngoài để cho phần tử nổi lên 
     {
-        for(int k=0;k<n-1;k++)
-            if(a[k]<a[k+1])
-                swap(a[k],a[k+1]);
+        for(int k=0;k<n-1;k++)    //Chạy n-1 lần vì dưới đk có a[k+1] nếu để <n thì sẽ xảy ra lỗi
+            if(a[k]<a[k+1])       // So sánh 2 phần tử liên tiếp trong mảng
+                Swap(a[k],a[k+1]);
     }
 }
